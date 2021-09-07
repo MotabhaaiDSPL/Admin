50cf484bf5e58c7b42de62f148b2c610945da6d9
orisonfonsecamotabhaai <orison.fonseca@motabhaai.com>
2021-09-07 14:56:37 +0530

    second

---
+++ src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/home/orison/Documents/React/my-project/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','dva','getInitialState','initialStateConfig','request',],
+});
+
+export { plugin };
